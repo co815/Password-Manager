57c42c89eff572fca2b1d3f19aabf1bd64ecd16d
Sandor Stefan <stefansandor24.08@gmail.com>
2025-09-16 14:31:10 +0300

    - am inceput sa lucrez la credentiale

--- backend/src/main/java/com/example/pm/credential/CredentialController.java
+++ backend/src/main/java/com/example/pm/credential/CredentialController.java
@@ -1,26 +1,58 @@
 package com.example.pm.credential;
 
+import com.example.pm.dto.CredentialDtos;
+import com.example.pm.model.Credential;
 import com.example.pm.repo.CredentialRepository;
+import com.example.pm.security.JwtService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController @RequestMapping("/api/credentials")
 public class CredentialController {
 
     private final CredentialRepository credentialRepository;
+    private final JwtService jwt;
 
-    public CredentialController(CredentialRepository credentialRepository) {
+    public CredentialController(CredentialRepository credentialRepository,  JwtService jwt) {
         this.credentialRepository = credentialRepository;
+        this.jwt = jwt;
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllCredentialsByUserId(@RequestParam String userId) {
-        return ResponseEntity.ok(credentialRepository.findAll());
+    public ResponseEntity<?> getAllCredentialsByUserId(Authentication authentication) {
+
+        String userID = (String) authentication.getPrincipal();
+        List<Credential> credentials = credentialRepository.findByUserId(userID);
+
+        return ResponseEntity.ok(new CredentialDtos.GetAllCredentialResponse(credentials));
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getCredentialByUserId(@RequestParam String userId) {
-        return ResponseEntity.ok(credentialRepository.findById(userId));
+    public ResponseEntity<?> getCredentialByUserId(Authentication authentication, @PathVariable String id) {
+
+        String userID = (String) authentication.getPrincipal();
+
+        return
+
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addCredential(CredentialDtos.RegisterCredentialRequest registerRequest) {
+        return ResponseEntity.ok(credentialRepository.save(new Credential(registerRequest)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCredential(CredentialDtos.ModifyCredentialRequest modifyRequest) {
+        return  ResponseEntity.ok();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCredential(CredentialDtos.DeleteCredentialRequest deleteRequest) {
+
     }
 
 }
--- backend/src/main/java/com/example/pm/dto/CredentialDtos.java
+++ backend/src/main/java/com/example/pm/dto/CredentialDtos.java
@@ -1,4 +1,35 @@
 package com.example.pm.dto;
 
+import com.example.pm.model.Credential;
+
+import java.util.List;
+
 public class CredentialDtos {
+
+    public record PublicCredential(
+            String credentialId,
+            String service,
+            String website,
+            String username,
+            String passwordEncrypted,
+            String passwordNonce
+    ) {
+        public static PublicCredential fromCredential(Credential credential){
+            return new PublicCredential(
+                    credential.getId(),
+                    credential.getService(),
+                    credential.getWebsite(),
+                    credential.getUsername(),
+                    credential.getPasswordEncrypted(),
+                    credential.getPasswordNonce()
+            );
+        }
+    }
+
+    // Get credential
+    public record GetAllCredentialResponse(
+            List<PublicCredential> credentials
+    ) {}
+
+
 }
